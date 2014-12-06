9a1ebea378632e1a10a5863aab65160219fed9ad
arya <aboudaie@brandeis.edu>
2014-12-06 12:17:32 -0500

    Yay the request now gets the HTML

--- brandeis_points/points_request2.py
+++ brandeis_points/points_request2.py
@@ -4,31 +4,26 @@ import urllib, urllib2
 import secrets
 
 import requests
+from random import randrange
+def get_html(username,password):
+    cj = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    opener.addheaders = [('User-agent', 'BrandeisPoints')]
+    urllib2.install_opener(opener)
+    authentication_url = 'https://login.brandeis.edu/cgi-bin/cosign.cgi'
+    payload = {}
+    payload['required']="BRANDEIS.EDU,ldapauth"
+    payload['ref']="https://apps.brandeis.edu//offsitesso/cbord"
+    payload['service']="cosign-OffsiteSSO-apps-prod"
+    payload["login"]=secrets.brandeis_user
+    payload["password"]=secrets.brandeis_pass
+    payload["doLogin.x"]=randrange(1,99)
+    payload["doLogin.y"]=randrange(1,99)
 
+    s = requests.session()
+    login_data = payload
+    s.get('https://login.brandeis.edu/cgi-bin/cosign.cgi')
+    s.post('https://login.brandeis.edu/cgi-bin/cosign.cgi', data=login_data)
+    r = s.get('https://brandeis.netcardmanager.com/student/welcome.php', verify=False)
 
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-opener.addheaders = [('User-agent', 'BrandeisPoints')]
-urllib2.install_opener(opener)
-authentication_url = 'https://login.brandeis.edu/cgi-bin/cosign.cgi'
-payload = {}
-payload['required']="BRANDEIS.EDU,ldapauth"
-payload['ref']="https://apps.brandeis.edu//offsitesso/cbord"
-payload['service']="cosign-OffsiteSSO-apps-prod"
-payload["login"]=secrets.brandeis_user
-payload["password"]=secrets.brandeis_pass
-payload["doLogin.x"]=56
-payload["doLogin.y"]=15
-
-data = urllib.urlencode(payload)
-req = urllib2.Request(authentication_url, data)
-resp = urllib2.urlopen(req)
-contents = resp.read()
-#print contents
-
-s = requests.session()
-login_data = payload
-s.post('https://login.brandeis.edu/cgi-bin/cosign.cgi', data=login_data)
-r = s.get('https://brandeis.netcardmanager.com/student/welcome.php')
-
-print r.content
+    return r.content
