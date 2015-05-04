919511b0dac3290b0fbf6a6d06044dbc2948fe57
arya <aboudaie@brandeis.edu>
2015-05-03 20:02:37 -0400

    random push

--- points/views.py
+++ points/views.py
@@ -5,7 +5,10 @@ import json
 # Create your views here.
 
 def get_meals(request):
-    #if request.method=='POST':
     data = get_html(secrets.brandeis_user,secrets.brandeis_pass)
     d = {"points":data[0], "meals":data[1]}
     return HttpResponse(json.dumps(d), content_type="application/json")
+
+def config(request):
+    if request.method=='POST':
+        pass
