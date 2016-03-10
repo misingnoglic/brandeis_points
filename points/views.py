987f80a82650b13db69ab714ff481b210537e2c1
arya <aboudaie@brandeis.edu>
2016-03-11 01:47:11 +0530

    Added some error handling

--- points/views.py
+++ points/views.py
@@ -7,9 +7,15 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def get_meals(request):
     if request.method == 'POST':
-            brandeis_user = request.POST["username"].lower()
-            brandeis_pass = request.POST["password"]
-    
+        
+        brandeis_user = request.POST["username"].lower()
+        brandeis_pass = request.POST["password"]
+        try:
             data = get_html(brandeis_user,brandeis_pass)
-            d = {"points":data[0], "meals":data[1]}
-            return HttpResponse(json.dumps(d), content_type="application/json")
+            d = {"success":True,"points":data[0], "meals":data[1]}
+        except ConnectionError: # Bad Login
+            d = {"success":False, "error":"Incorrect Credentials"}
+        
+    else:
+        d = {"success":False, "error":"Not Valid POST REQUEST"}
+    return HttpResponse(json.dumps(d), content_type="application/json")
